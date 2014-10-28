4ef699c9e47fedfae3c9b03766b02f04795cb5f8
vagmat <v4gm4t@hotmail.com>
2014-10-28 15:40:30 +0200

    adding comments

---
+++ Task1URL.java
@@ -0,0 +1,30 @@
+
+import java.io.IOException;
+//javax.swing provides a variety of usefullcomponents//
+//Imports a text component that can edit various kinds of content//
+import javax.swing.JEditorPane;
+//Adds atop-level window with th an title and a border (a frame basicaly) to your content//
+import javax.swing.JFrame;
+//Converts the browsers scrollbar into your content projection//
+import javax.swing.JScrollPane;
+
+public class Task1URL  {
+	public static void main(String[] args) {
+		JEditorPane jep = new JEditorPane();
+		jep.setEditable(false);
+		try {
+		  jep.setPage("http://en.wikipedia.org/wiki/Java_(programming_language)");
+		}
+		catch (IOException e) {
+		  jep.setContentType("text/html");
+		  jep.setText("<html>Could not load</html>");
+		} //the condition of the exception//
+		JScrollPane scrollPane = new JScrollPane(jep);
+		JFrame f = new JFrame("Site Projection");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.getContentPane().add(scrollPane);
+		//creates projection window whith the scrollpane wher jep=jEditorpane//
+
+		f.setVisible(true);
+    }
+}
---
+++ URLProgram.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+
+public class URLProgram {
+	public static void main(String[] args) throws Exception {
+		URL programmingWikipedia = new URL("http://el.wikipedia.org/wiki/Java");
+		BufferedReader in = new BufferedReader(new InputStreamReader(programmingWikipedia.openStream()));
+		String input;
+		while ((input = in.readLine()) != null)
+            System.out.println(input);
+        in.close();
+		
+	}
+
+}
